20285fcad5e99494deb7594bde0e8d9e8249db3a
ashmitasur <68198650+ashmitasur@users.noreply.github.com>
2020-07-21 21:45:21 +0530

    Update ajax-util.js

--- Aax/js/ajax-util.js
+++ Aax/js/ajax-util.js
@@ -1,22 +1,52 @@
-// Event handling
-document.addEventListener("DOMContentLoaded",
-  function (event) {
-    
-    // Unobtrusive event binding
-    document.querySelector("button")
-      .addEventListener("click", function () {
-        
-        // Call server to get the name
-        $ajaxUtils
-          .sendGetRequest("data/name.txt", 
-            function (request) {
-              var name = request.responseText;
+(function (global) {
 
-              document.querySelector("#content")
-                .innerHTML = "<h2>Hello " + name + "!</h2>";
-            });
+// Set up a namespace for our utility
+var ajaxUtils = {};
 
-        
-      });
+
+// Returns an HTTP request object
+function getRequestObject() {
+  if (global.XMLHttpRequest) {
+    return (new XMLHttpRequest());
+  } 
+  else if (global.ActiveXObject) {
+    // For very old IE browsers (optional)
+    return (new ActiveXObject("Microsoft.XMLHTTP"));
+  } 
+  else {
+    global.alert("Ajax is not supported!");
+    return(null); 
   }
-);
+}
+
+
+// Makes an Ajax GET request to 'requestUrl'
+ajaxUtils.sendGetRequest = 
+  function(requestUrl, responseHandler) {
+    var request = getRequestObject();
+    request.onreadystatechange = 
+      function() { 
+        handleResponse(request, responseHandler); 
+      };
+    request.open("GET", requestUrl, true);
+    request.send(null); // for POST only
+  };
+
+
+// Only calls user provided 'responseHandler'
+// function if response is ready
+// and not an error
+function handleResponse(request,
+                        responseHandler) {
+  if ((request.readyState == 4) &&
+     (request.status == 200)) {
+    responseHandler(request);
+  }
+}
+
+
+// Expose utility to the global object
+global.$ajaxUtils = ajaxUtils;
+
+
+})(window);
